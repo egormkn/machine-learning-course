60fd609c5374a3c047c1f10c73c8ac1c13b1db51
Egor Makarenko <egormkn@yandex.ru>
2019-03-07 18:32:09 +0300

    Update cross-validation

--- 01-cross-validation/main.cpp
+++ 01-cross-validation/main.cpp
@@ -1,37 +1,63 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 using namespace std;
 
-int main() {
+void solve() {
+    /**
+     * n - number of objects
+     * m - number of classes
+     * k - number of parts
+     */
     unsigned n, m, k;
-    scanf("%u %u %u", &n, &m, &k);
+    cin >> n >> m >> k;
 
     vector<vector<int>> classes(m), parts(k);
 
-    for (unsigned i = 0, c; i < n; i++) {
-        scanf("%u", &c);
-        classes[--c].push_back(i);
+    for (unsigned object = 0, class_id; object < n; object++) {
+        cin >> class_id;
+        classes[--class_id].push_back(object);
     }
 
-    int p = 0;
+    int current_part = 0;
     for (auto &cls : classes) {
         auto size = static_cast<unsigned>(cls.size());
         auto block_size = size / k;
         for (int i = 0; i < size; i++) {
             bool extra = i >= size - size % k;
-            parts[extra ? p : (i / block_size)].push_back(cls[i]);
-            if (extra) p = (p + 1) % k;
+            parts[extra ? current_part : (i / block_size)].push_back(cls[i]);
+            if (extra) current_part = (current_part + 1) % k;
         }
     }
 
     for (auto &part : parts) {
-        printf("%u ", static_cast<unsigned int>(part.size()));
-        for (auto &obj : part) {
-            printf("%u ", obj + 1);
+        auto size = static_cast<unsigned>(part.size());
+        cout << size << ' ';
+        for (auto &object : part) {
+            cout << object + 1 << ' ';
         }
-        printf("\n");
+        cout << endl;
     }
+}
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifdef DEBUG
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+    cin.rdbuf(input.rdbuf());
+    cout.rdbuf(output.rdbuf());
+#endif
+
+    solve();
+    cout.flush();
+
+#ifdef DEBUG
+    input.close();
+    output.close();
+#endif
     return 0;
 }
