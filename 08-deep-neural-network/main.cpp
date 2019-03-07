9afa15e69eb9b32bc50a1d4ac41e8d1d604e7c6a
Egor Makarenko <egormkn@yandex.ru>
2019-03-07 18:33:23 +0300

    Update code templates

--- 08-deep-neural-network/main.cpp
+++ 08-deep-neural-network/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+void solve() {
+    // TODO
+}
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifdef DEBUG
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+    cin.rdbuf(input.rdbuf());
+    cout.rdbuf(output.rdbuf());
+#endif
+
+    solve();
+    cout.flush();
+
+#ifdef DEBUG
+    input.close();
+    output.close();
+#endif
     return 0;
 }
