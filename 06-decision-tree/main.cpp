6e42f7e733c4598ec4a9086d353be602cc9cddae
Egor Makarenko <egormkn@yandex.ru>
2019-03-12 01:53:21 +0300

    Add decision tree

--- 06-decision-tree/main.cpp
+++ 06-decision-tree/main.cpp
@@ -16,18 +16,17 @@ typedef unsigned class_t;
 
 typedef pair<unsigned, double> predicate_t;
 
+typedef tuple<feature_t, class_t, size_t> feature_value_t;
+
 class object {
 private:
-    size_t index;
     vector<feature_t> features;
     class_t class_id;
 public:
-    object(size_t index, vector<feature_t> features, class_t class_id) :
-            index(index), features(move(features)), class_id(class_id) {}
+    object(vector<feature_t> features, class_t class_id) :
+            features(move(features)), class_id(class_id) {}
 
     size_t size() const { return features.size(); }
-
-    size_t get_index() const { return index; }
 
     feature_t &operator[](size_t index) { return features[index]; }
 
@@ -37,6 +36,7 @@ public:
 };
 
 namespace gain {
+
     int gini(const vector<object> &objects, const function<bool(const object &)> &predicate_fn) {
         int score = 0;
         for (int i = 0; i < objects.size(); ++i) {
@@ -79,20 +79,26 @@ namespace gain {
         return score;
     }
 
-    double entropy(const vector<pair<unsigned, unsigned>> &distribution) {
+    double entropy(const vector<object> &objects, const function<bool(const object &)> &predicate_fn) {
         double result = 0.0;
-
-        int true_size = 0, false_size = 0;
-        for (auto &pair : distribution) {
-            false_size += pair.first;
-            true_size += pair.second;
+        vector<pair<unsigned, unsigned>> distribution;
+        unsigned true_objects = 0;
+        for (const object &object : objects) {
+            class_t obj_class = object.get_class();
+            if (obj_class >= distribution.size()) distribution.resize(obj_class + 1);
+            if (predicate_fn(object)) {
+                distribution[obj_class].second++;
+                true_objects++;
+            } else {
+                distribution[obj_class].first++;
+            }
         }
 
         auto h = [](double z) { return z < 1e-10 ? 0 : -z * log2(z); };
         auto P = [&distribution](class_t c) { return distribution[c].first + distribution[c].second; };
         auto p = [&distribution](class_t c) { return distribution[c].second; };
-        double p_all = true_size;
-        double l = true_size + false_size;
+        double p_all = true_objects;
+        double l = objects.size();
         const double epsilon = 1e-10;
 
         for (class_t c = 0; c < distribution.size(); c++) {
@@ -108,38 +114,20 @@ namespace gain {
 
 class decision_tree {
 public:
-    static decision_tree make_tree(size_t features_size, size_t classes_size, const vector<object> &train_set) {
-        const size_t m = features_size;
-        const size_t k = classes_size;
-        const size_t n = train_set.size();
 
-        // Collect feature values along with the classes they represent
-        vector<vector<pair<feature_t, class_t>>> feature_values(m);
-        for (size_t feature_id = 0; feature_id < m; ++feature_id) {
-            for (const object &object : train_set) {
-                feature_values[feature_id].emplace_back(object[feature_id], object.get_class());
+    static decision_tree make_tree(size_t features_size, size_t classes_size, const vector<object> &train_set) {
+
+        vector<vector<feature_value_t>> feature_values(features_size);
+        for (size_t feature_id = 0; feature_id < features_size; ++feature_id) {
+            for (size_t object_id = 0; object_id < train_set.size(); ++object_id) {
+                const object &object = train_set[object_id];
+                feature_values[feature_id].emplace_back(object[feature_id], object.get_class(), object_id);
             }
             sort(feature_values[feature_id].begin(), feature_values[feature_id].end());
         }
 
-        // Create threshold lists for use in rules
-        vector<vector<double>> thresholds(m);
-        for (size_t feature_id = 0; feature_id < m; ++feature_id) {
-            auto &feature_values_vector = feature_values[feature_id];
-            for (int i = 1; i < feature_values_vector.size(); ++i) {
-                const auto &previous = feature_values_vector[i - 1];
-                const auto &current = feature_values_vector[i];
-                bool class_changed = previous.second != current.second;
-                if (class_changed && fabs(previous.first - current.first) > epsilon) {
-                    double threshold = (previous.first + current.first) / 2.0f;
-                    thresholds[feature_id].push_back(threshold);
-                }
-            }
-        }
-
-        decision_tree root = build_tree(train_set, thresholds, 0, classes_size);
-
-        return root;
+        decision_tree_builder builder(classes_size, train_set);
+        return builder.build_tree(0, train_set, feature_values);
     }
 
     size_t size() const { return total_size; }
@@ -205,150 +193,156 @@ private:
             class_id(class_id),
             predicate() {}
 
-    static auto get_predicate_fn(predicate_t predicate) {
-        return [predicate](const object &object) {
-            return object[predicate.first] < predicate.second;
-        };
-    }
+    class decision_tree_builder {
+    private:
+        const size_t classes_size;
+        const vector<object> objects;
 
-    static double get_entropy(const vector<unsigned> &distribution, size_t objects_size) {
-        double result = 0.0;
-        for (class_t c = 0; c < distribution.size(); ++c) {
-            if (distribution[c] > 0) {
-                double z = (double) distribution[c] / objects_size;
-                result += -z * log2(z);
+        vector<unsigned> class_distribution(const vector<object> &objects) {
+            vector<unsigned> distribution(classes_size);
+            for (const auto &object : objects) {
+                ++distribution[object.get_class()];
             }
-        }
-        return result;
-    }
-
-    static vector<unsigned> class_distribution(const vector<object> &objects, size_t classes_size) {
-        vector<unsigned> distribution(classes_size);
-        for (const auto &object : objects) {
-            distribution[object.get_class()]++;
-        }
-        return distribution;
-    }
-
-    static class_t most_common_class(const vector<object> &objects, size_t classes_size) {
-        vector<unsigned> distribution = class_distribution(objects, classes_size);
-        return most_common_class(distribution);
-    }
-
-    static class_t most_common_class(const vector<unsigned> &distribution) {
-        auto max_iterator = max_element(distribution.begin(), distribution.end());
-        return static_cast<class_t>(distance(distribution.begin(), max_iterator));
-    }
-
-    static decision_tree build_tree(const vector<object> &objects,
-                                    const vector<vector<double>> &thresholds,
-                                    int level, size_t classes_size) {
-        auto distribution = class_distribution(objects, classes_size);
-
-        if (level == max_level || get_entropy(distribution, objects.size()) < epsilon) {
-            return decision_tree(most_common_class(distribution));
+            return distribution;
         }
 
-        // Check if all objects belong to one class
-        auto not_equal_classes = [](const object &a, const object &b) {
-            return a.get_class() != b.get_class();
-        };
-        if (adjacent_find(objects.begin(), objects.end(), not_equal_classes) == objects.end()) {
-            class_t only_class = objects[0].get_class();
-            return decision_tree(only_class);
+        class_t most_common_class(const vector<unsigned> &distribution) {
+            auto max_iterator = max_element(distribution.begin(), distribution.end());
+            return static_cast<class_t>(distance(distribution.begin(), max_iterator));
         }
 
-        // Select predicate
-        predicate_t predicate = select_predicate_v2(objects, thresholds, distribution);
-        auto predicate_fn = get_predicate_fn(predicate);
+        predicate_t select_predicate(const vector<vector<feature_value_t>> &feature_values,
+                                     const vector<unsigned> &distribution) {
+            double best_gain = numeric_limits<double>::min();
+            predicate_t best_predicate = null_predicate;
 
-        if (predicate == null_predicate) {
-            return decision_tree(most_common_class(distribution));
-        }
+            for (unsigned feature_id = 0; feature_id < feature_values.size(); ++feature_id) {
+                const auto &fixed_feature_values = feature_values[feature_id];
+                if (fixed_feature_values.size() < 2) continue;
 
-        // Split objects by predicate
-        vector<object> left_objects, right_objects;
-        for (const object &object : objects) {
-            if (predicate_fn(object)) {
-                right_objects.push_back(object);
-            } else {
-                left_objects.push_back(object);
-            }
-        }
+                size_t true_size = 0, false_size = fixed_feature_values.size();
+                vector<pair<unsigned, unsigned>> predicate_distribution(classes_size);
+                for (size_t class_id = 0; class_id < classes_size; ++class_id) {
+                    predicate_distribution[class_id].first = distribution[class_id];
+                }
 
-        if (left_objects.empty() || right_objects.empty()) {
-            return decision_tree(most_common_class(distribution));
-        }
+                for (size_t i = 1; i < fixed_feature_values.size(); ++i) {
+                    const auto &previous_value = get<0>(fixed_feature_values[i - 1]);
+                    const auto &previous_class = get<1>(fixed_feature_values[i - 1]);
+                    const auto &current_value = get<0>(fixed_feature_values[i]);
+                    const auto &current_class = get<1>(fixed_feature_values[i]);
 
-        // Split thresholds
-        vector<vector<double>> left_thresholds = thresholds, right_thresholds = thresholds;
-        auto &left_modified = left_thresholds[predicate.first];
-        auto &right_modified = right_thresholds[predicate.first];
-        auto left_filter = [t = predicate.second](double value) { return value < t; };
-        auto right_filter = [t = predicate.second](double value) { return value >= t; };
-        left_modified.erase(remove_if(left_modified.begin(), left_modified.end(), left_filter), left_modified.end());
-        right_modified.erase(remove_if(right_modified.begin(), right_modified.end(), right_filter),
-                             right_modified.end());
+                    // Add previous value
+                    true_size++;
+                    false_size--;
+                    predicate_distribution[previous_class].first--;
+                    predicate_distribution[previous_class].second++;
 
-        decision_tree left = build_tree(left_objects, left_thresholds, level + 1, classes_size);
-        decision_tree right = build_tree(right_objects, right_thresholds, level + 1, classes_size);
+                    if (previous_class == current_class || fabs(previous_value - current_value) < epsilon) continue;
 
-        return decision_tree(left, right, predicate);
-    }
+                    double threshold = (previous_value + current_value) / 2.0f;
 
-    /*static predicate_t select_predicate(const vector<object> &objects, const vector<vector<double>> &thresholds) {
-        double best_gain = 0.0;
-        predicate_t best_predicate = null_predicate;
+                    auto h = [](double z) { return z < 1e-10 ? 0 : -z * log2(z); };
+                    auto P = [&predicate_distribution](class_t c) {
+                        return predicate_distribution[c].first + predicate_distribution[c].second;
+                    };
+                    auto p = [&predicate_distribution](class_t c) {
+                        return predicate_distribution[c].second;
+                    };
+                    double p_all = true_size;
+                    double l = true_size + false_size;
 
-        for (unsigned feature_id = 0; feature_id < thresholds.size(); ++feature_id) {
-            for (double threshold : thresholds[feature_id]) {
-                predicate_t predicate(feature_id, threshold);
-                auto predicate_fn = get_predicate_fn(predicate);
-                double gain = gain::entropy(objects, predicate_fn);
-                if (gain > best_gain) {
-                    best_gain = gain;
-                    best_predicate = predicate;
+                    double entropy_gain = 0.0;
+                    for (class_t c = 0; c < predicate_distribution.size(); c++) {
+                        if (fabs(l) < epsilon || fabs(p_all) < epsilon || fabs(l - p_all) < epsilon) continue;
+                        double sum_1 = h(P(c) / l);
+                        double sum_2 = (p_all / l) * h(p(c) / p_all);
+                        double sum_3 = ((l - p_all) / l) * h((P(c) - p(c)) / (l - p_all));
+                        entropy_gain += sum_1 - sum_2 - sum_3;
+                    }
+
+                    if (entropy_gain > best_gain) {
+                        best_gain = entropy_gain;
+                        best_predicate = make_pair(feature_id, threshold);
+                    }
                 }
             }
+
+            cerr << "New predicate: A[" << best_predicate.first << "] < " << best_predicate.second << ", gain = "
+                 << best_gain << endl << endl;
+
+            return best_predicate;
         }
-        return best_predicate;
-    }*/
 
-    static predicate_t select_predicate_v2(const vector<object> &objects, const vector<vector<double>> &thresholds,
-                                           const vector<unsigned> &distribution) {
-        double best_gain = 0.0;
-        predicate_t best_predicate = null_predicate;
+    public:
+        decision_tree_builder(const size_t classes_size, vector<object> objects) :
+                classes_size(classes_size),
+                objects(move(objects)) {}
 
-        for (unsigned feature_id = 0; feature_id < thresholds.size(); ++feature_id) {
-            vector<object> sorted_objects = objects;
-            sort(sorted_objects.begin(), sorted_objects.end(), [feature_id](const object &a, const object &b) {
-                return a[feature_id] < b[feature_id];
-            });
-            int obj_index = 0;
-            vector<pair<unsigned, unsigned>> pred_distribution(distribution.size());
-            transform(distribution.begin(), distribution.end(), pred_distribution.begin(), [](unsigned value) {
-                return make_pair(value, 0);
-            });
-            for (double threshold : thresholds[feature_id]) {
-                predicate_t predicate(feature_id, threshold);
-                auto predicate_fn = get_predicate_fn(predicate);
+        decision_tree build_tree(int level, const vector<object> &current_objects,
+                                 const vector<vector<feature_value_t>> &feature_values) {
+            const size_t features_size = feature_values.size();
 
-                while (obj_index < sorted_objects.size() && sorted_objects[obj_index][feature_id] < threshold) {
-                    const object &object = sorted_objects[obj_index];
-                    obj_index++;
-                    auto &pair = pred_distribution[object.get_class()];
-                    pair.second++;
-                    pair.first--;
-                }
-                double gain = gain::entropy(pred_distribution);
-                if (gain > best_gain) {
-                    best_gain = gain;
-                    best_predicate = predicate;
+            auto distribution = class_distribution(current_objects);
+
+            if (level == max_level) {
+                return decision_tree(most_common_class(distribution));
+            }
+
+            // Check if all objects belong to one class
+            auto not_equal_classes = [](const object &a, const object &b) {
+                return a.get_class() != b.get_class();
+            };
+            if (adjacent_find(current_objects.begin(), current_objects.end(), not_equal_classes) ==
+                current_objects.end()) {
+                class_t only_class = current_objects[0].get_class();
+                return decision_tree(only_class);
+            }
+
+            // Select predicate
+            predicate_t predicate = select_predicate(feature_values, distribution);
+            auto predicate_fn = [&predicate](const object &object) {
+                return object[predicate.first] < predicate.second;
+            };
+
+            if (predicate == null_predicate) {
+                return decision_tree(most_common_class(distribution));
+            }
+
+            // Split objects by predicate
+            vector<vector<feature_value_t>> left_feature_values(features_size), right_feature_values(features_size);
+            for (size_t feature_id = 0; feature_id < features_size; ++feature_id) {
+                for (const auto &feature : feature_values[feature_id]) {
+                    size_t index = get<2>(feature);
+                    if (predicate_fn(objects[index])) {
+                        right_feature_values[feature_id].push_back(feature);
+                    } else {
+                        left_feature_values[feature_id].push_back(feature);
+                    }
                 }
             }
+
+            size_t left_objects_size = left_feature_values[0].size(), right_objects_size = right_feature_values[0].size();
+
+            if (left_objects_size == 0 || right_objects_size == 0) {
+                return decision_tree(most_common_class(distribution));
+            }
+
+            vector<object> left_objects, right_objects;
+            for (const object &object : current_objects) {
+                if (predicate_fn(object)) {
+                    right_objects.push_back(object);
+                } else {
+                    left_objects.push_back(object);
+                }
+            }
+
+            decision_tree left = build_tree(level + 1, left_objects, left_feature_values);
+            decision_tree right = build_tree(level + 1, right_objects, right_feature_values);
+
+            return decision_tree(left, right, predicate);
         }
-        return best_predicate;
-    }
+    };
 };
 
 void solve() {
@@ -370,7 +364,7 @@ void solve() {
         }
         class_t class_id;
         cin >> class_id;
-        train_set.emplace_back(object_id, features, class_id - 1);
+        train_set.emplace_back(features, class_id - 1);
     }
 
     auto tree = decision_tree::make_tree(m, k, train_set);
@@ -384,16 +378,25 @@ int main() {
 #ifdef DEBUG
     ifstream input("input.txt");
     ofstream output("output.txt");
+    streambuf *cin_buffer(cin.rdbuf());
+    streambuf *cout_buffer(cout.rdbuf());
     cin.rdbuf(input.rdbuf());
     cout.rdbuf(output.rdbuf());
+#else
+    streambuf *cerr_buffer(cerr.rdbuf());
+    cerr.rdbuf(nullptr);
 #endif
 
     solve();
     cout.flush();
 
 #ifdef DEBUG
+    cin.rdbuf(cin_buffer);
+    cout.rdbuf(cout_buffer);
     input.close();
     output.close();
+#else
+    cerr.rdbuf(cerr_buffer);
 #endif
     return 0;
 }
