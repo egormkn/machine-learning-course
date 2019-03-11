ecdda284b9b6f34d6cbe1feaf5a183c7622849c9
Egor Makarenko <egormkn@yandex.ru>
2019-03-12 01:58:43 +0300

    Update main function template

--- 02-f1-score/main.cpp
+++ 02-f1-score/main.cpp
@@ -53,16 +53,25 @@ int main() {
 #ifdef DEBUG
     ifstream input("input.txt");
     ofstream output("output.txt");
+    streambuf *cin_buffer(cin.rdbuf());
+    streambuf *cout_buffer(cout.rdbuf());
     cin.rdbuf(input.rdbuf());
     cout.rdbuf(output.rdbuf());
+#else
+    streambuf *cerr_buffer(cerr.rdbuf());
+    cerr.rdbuf(nullptr);
 #endif
 
     solve();
     cout.flush();
 
 #ifdef DEBUG
+    cin.rdbuf(cin_buffer);
+    cout.rdbuf(cout_buffer);
     input.close();
     output.close();
+#else
+    cerr.rdbuf(cerr_buffer);
 #endif
     return 0;
 }
--- 09-logic-expression/main.cpp
+++ 09-logic-expression/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 
 using namespace std;
 
@@ -15,16 +14,25 @@ int main() {
 #ifdef DEBUG
     ifstream input("input.txt");
     ofstream output("output.txt");
+    streambuf *cin_buffer(cin.rdbuf());
+    streambuf *cout_buffer(cout.rdbuf());
     cin.rdbuf(input.rdbuf());
     cout.rdbuf(output.rdbuf());
+#else
+    streambuf *cerr_buffer(cerr.rdbuf());
+    cerr.rdbuf(nullptr);
 #endif
 
     solve();
     cout.flush();
 
 #ifdef DEBUG
+    cin.rdbuf(cin_buffer);
+    cout.rdbuf(cout_buffer);
     input.close();
     output.close();
+#else
+    cerr.rdbuf(cerr_buffer);
 #endif
     return 0;
 }
