0b5479fb1b30266f58520ac001ff00c9f4c9de41
Egor Makarenko <egormkn@yandex.ru>
2019-03-22 02:30:58 +0300

    Add chi-square

--- 10-chi-square/main.cpp
+++ 10-chi-square/main.cpp
@@ -1,10 +1,55 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <iomanip>
 
 using namespace std;
 
 void solve() {
-    // TODO
+    /**
+     * k1 - number of unique values of feature 1
+     * k2 - number of unique values of feature 2
+     * n - number of objects
+     */
+    size_t rows_size, columns_size, objects_size;
+    cin >> rows_size >> columns_size >> objects_size;
+
+    map<pair<unsigned, unsigned>, unsigned> contingency_table;
+    vector<unsigned> row_sum(rows_size), column_sum(columns_size);
+
+    for (unsigned i = 0; i < objects_size; ++i) {
+        unsigned row, column;
+        cin >> row >> column;
+        --row;
+        --column;
+        auto key = make_pair(row, column);
+        ++contingency_table[key];
+        ++row_sum[row];
+        ++column_sum[column];
+    }
+
+    double answer = 0.0;
+    vector<unsigned> row_present_columns_sum(rows_size, 0);
+
+    // Add non-zero cells to answer
+    for (const auto &[key, value] : contingency_table) {
+        auto[row, column] = key;
+        double observed_frequency = value;
+        double expected_frequency = row_sum[row] * column_sum[column] / static_cast<double>(objects_size);
+        if (expected_frequency > 0) {
+            answer += pow(observed_frequency - expected_frequency, 2) / expected_frequency;
+        }
+        row_present_columns_sum[row] += column_sum[column];
+    }
+
+    // Add zero cells to answer
+    for (unsigned row = 0; row < rows_size; ++row) {
+        answer += row_sum[row] * (objects_size - row_present_columns_sum[row]) / static_cast<double>(objects_size);
+    }
+
+    cout << setprecision(10) << answer;
 }
 
 int main() {
@@ -23,6 +68,7 @@ int main() {
     cerr.rdbuf(nullptr);
 #endif
 
+    cout << fixed;
     solve();
     cout.flush();
 
