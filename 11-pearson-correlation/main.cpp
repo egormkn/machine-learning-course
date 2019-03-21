128047188ea9eac700460d3b2684736cc7b22066
Egor Makarenko <egormkn@yandex.ru>
2019-03-22 02:57:10 +0300

    Add Pearson correlation

--- 11-pearson-correlation/main.cpp
+++ 11-pearson-correlation/main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 void solve() {
-    // TODO
+    /**
+     * n - number of objects
+     */
+    unsigned objects_size;
+    cin >> objects_size;
+
+    vector<double> x(objects_size), y(objects_size);
+    double x_mean = 0.0, y_mean = 0.0;
+
+    for (unsigned i = 0; i < objects_size; ++i) {
+        cin >> x[i] >> y[i];
+        x_mean += x[i] / objects_size;
+        y_mean += y[i] / objects_size;
+    }
+
+    double numerator = 0, squares_sum_x = 0, squares_sum_y = 0;
+    for (unsigned i = 0; i < objects_size; ++i) {
+        numerator += (x[i] - x_mean) * (y[i] - y_mean);
+        squares_sum_x += pow(x[i] - x_mean, 2);
+        squares_sum_y += pow(y[i] - y_mean, 2);
+    }
+
+    double denominator = sqrt(squares_sum_x * squares_sum_y);
+
+    cout << setprecision(10) << (denominator > 0 ? numerator / denominator : 0.0);
 }
 
 int main() {
@@ -23,6 +50,7 @@ int main() {
     cerr.rdbuf(nullptr);
 #endif
 
+    cout << fixed;
     solve();
     cout.flush();
 
