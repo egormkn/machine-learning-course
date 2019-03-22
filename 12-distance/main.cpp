37415cee0c61f16a113b0877c81e15e178f0b0e2
Egor Makarenko <egormkn@yandex.ru>
2019-03-22 03:39:07 +0300

    Add class distance

--- 12-distance/main.cpp
+++ 12-distance/main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+long long distance(vector<int> objects) {
+    sort(objects.begin(), objects.end());
+
+    long long result = 0;
+    for (long long i = 0; i < objects.size(); ++i) {
+        result += i * objects[i] - (objects.size() - 1 - i) * objects[i];
+    }
+
+    return result * 2;
+}
+
 void solve() {
-    // TODO
+    /**
+     * k - number of unique values of feature y
+     * n - number of objects
+     */
+    unsigned k, n;
+    cin >> k >> n;
+
+    vector<int> objects;
+    vector<vector<int>> grouped_objects(k);
+
+    for (unsigned i = 0; i < n; ++i) {
+        int x;
+        unsigned y;
+        cin >> x >> y;
+        objects.push_back(x);
+        grouped_objects[y - 1].push_back(x);
+    }
+
+    long long intra_class_distance = 0;
+    for (const vector<int> &intra_class : grouped_objects) {
+        intra_class_distance += distance(intra_class);
+    }
+    long long inter_class_distance = distance(objects) - intra_class_distance;
+
+    cout << intra_class_distance << endl;
+    cout << inter_class_distance << endl;
 }
 
 int main() {
@@ -23,6 +61,7 @@ int main() {
     cerr.rdbuf(nullptr);
 #endif
 
+    cout << fixed;
     solve();
     cout.flush();
 
