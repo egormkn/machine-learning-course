1a50b151dfba58e0cfc6fd1e8007e924df4f0cd4
Egor Makarenko <egormkn@yandex.ru>
2019-03-22 04:00:16 +0300

    Add conditional entropy

--- 13-conditional-entropy/main.cpp
+++ 13-conditional-entropy/main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <iomanip>
 
 using namespace std;
 
 void solve() {
-    // TODO
+    /**
+     * kx - number of unique values of feature x
+     * ky - number of unique values of feature y
+     * n - number of objects
+     */
+    unsigned rows_size, columns_size, objects_size;
+    cin >> rows_size >> columns_size >> objects_size;
+
+    map<pair<unsigned, unsigned>, unsigned> contingency_table;
+
+    vector<unsigned> row_sum(rows_size);
+
+    for (unsigned i = 0; i < objects_size; ++i) {
+        unsigned row, column;
+        cin >> row >> column;
+        --row;
+        --column;
+        auto key = make_pair(row, column);
+        ++contingency_table[key];
+        ++row_sum[row];
+    }
+
+    double answer = 0.0;
+    for (const auto &[key, value] : contingency_table) {
+        auto[i, j] = key;
+        if (value == 0 || row_sum[i] == 0) continue;
+        double p = value / static_cast<double>(objects_size);
+        double log_p = log(value / static_cast<double>(row_sum[i]));
+        answer -= p * log_p;
+    }
+
+    cout << setprecision(10) << answer << endl;
 }
 
 int main() {
@@ -23,6 +58,7 @@ int main() {
     cerr.rdbuf(nullptr);
 #endif
 
+    cout << fixed;
     solve();
     cout.flush();
 
